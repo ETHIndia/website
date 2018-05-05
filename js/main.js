0d44ca22d88e81e4c6c4b078fce7dfe443d3527f
Akash Nimare <svnitakash@gmail.com>
2018-05-05 21:36:48 +0530

    Update navbar scroll position.

--- js/main.js
+++ js/main.js
@@ -23,7 +23,7 @@
       if (target.length) {
         $("html, body").animate(
           {
-            scrollTop: target.offset().top
+            scrollTop: target.offset().top - 100
           },
           1000
         );
