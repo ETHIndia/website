6696e901019207b075397ba9e5cc23c45f81e0b7
Abhishek Warokar <abhishek11@live.in>
2018-05-27 18:05:31 +0530

    :sparkles: udpates

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
-(function($) {
+(function ($) {
   "use strict"; // Start of use strict
   // Smooth scrolling using jQuery easing
 
-  $(window).scroll(function(event) {
+  $(window).scroll(function (event) {
     var scroll = $(window).scrollTop();
     // Do something
     if (scroll > 100) {
@@ -12,17 +12,16 @@
     }
   });
 
-  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
     if (
       location.pathname.replace(/^\//, "") ==
-        this.pathname.replace(/^\//, "") &&
+      this.pathname.replace(/^\//, "") &&
       location.hostname == this.hostname
     ) {
       var target = $(this.hash);
       target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
       if (target.length) {
-        $("html, body").animate(
-          {
+        $("html, body").animate({
             scrollTop: target.offset().top
           },
           1000
@@ -37,7 +36,7 @@
   // var navBgColor = $("#mainNav").css("background-color", "#363f47");
 
   // Closes responsive menu when a scroll trigger link is clicked
-  $(".js-scroll-trigger").click(function() {
+  $(".js-scroll-trigger").click(function () {
     if (window.innerWidth < "992") {
       $(".navbar-collapse").collapse("hide");
       $("#mainNav").css("background-color", "rgba(0, 0, 0, 0)");
@@ -50,10 +49,10 @@
     offset: 57
   });
 
-  $("#mainNav button").click(function() {
+  $("#mainNav button").click(function () {
     var navBgColor = $("#mainNav").css("background-color");
     if (navBgColor === "rgba(0, 0, 0, 0)") {
-      $("#mainNav").css("background-color", "#363f47");
+      $("#mainNav").css("background-color", "#111c33");
     } else {
       $("#mainNav").css("background-color", "rgba(0, 0, 0, 0)");
     }
